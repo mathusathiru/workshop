7172ebedc763714208550aad507913cda9d6781e
Mathusa Thiruchenthoor <mathu_thiru@icloud.com>
2026-01-04 19:27:25 +0000

    Update dice program for multiple dice

--- blink-led/dice.py
+++ blink-led/dice.py
@@ -4,12 +4,28 @@ from random import randint
 
 led = Pin("LED", Pin.OUT)
 
+def throw_dice(num_dice):
+    total = 0
+    for i in range(num_dice):
+        total += randint(1, 6)
+    return total
+
 def blink(times, delay):
     for i in range(times):
         led.on()
         sleep(delay)
         led.off()
         sleep(delay)
+
+while True:
+    try:
+        dice = int(input("Choose number of dice to roll with (1 to 5): "))
+        if dice > 5:
+            print("Too many dice - enter a number from 1 to 5\n")
+        else:
+            break
+    except ValueError:
+        print("Error: invalid input - enter a number from 1 to 5\n")
 
 print("Press Enter to roll dice or 'q' to quit: ")
 
@@ -21,6 +37,6 @@ while True:
     elif command != "":
         print("Error: Invalid input - press Enter to roll, or 'q' to quit")
     else:
-        dice_throw = randint(1, 6)
+        dice_throw = throw_dice(dice)
         print("You rolled a", dice_throw)
         blink(dice_throw, 0.2)
